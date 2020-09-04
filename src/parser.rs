e38ee2341ef366c26ade0e31172873c7b46bc76b
鐘妘甄 <shirley@mail.kway.com.tw>
2020-09-04 17:15:14 +0800

    complete version 1
    why:
    how:
    
    note:

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,10 @@
 use std::collections::LinkedList;
+use std::num::ParseIntError;
 
 /* 每個欄位的名稱及其長度 */
 pub struct Rec {
 	name: &'static str,
-	len : u32,
-}
-
-struct FieldPos {
-	begin: u32,
-	end  : u32,
+	len : usize,
 }
 
 #[allow(dead_code)]
@@ -21,26 +17,7 @@ fn init_header() -> LinkedList<Rec> {
 }
 
 #[allow(dead_code)]
-fn init_v2_ord_format() -> LinkedList<Rec> {
-	let mut lst = LinkedList::<Rec>::new();
-	lst.append(&mut init_header());
-	lst
-}
-
-fn get_field_pos_vec(lst: &LinkedList<Rec>) -> Vec<FieldPos> {
-	let mut vec = Vec::<FieldPos>::new();
-	let mut cur_begin : u32 = 0;
-	let mut cur_end   : u32 = 0;
-	for item in lst {
-		cur_begin = cur_end;
-		cur_end = cur_begin + item.len;
-		vec.push( FieldPos { begin: cur_begin, end: cur_end } );
-	}
-	vec
-}
-
-#[allow(dead_code)]
-fn init_v2_rpt_format() -> LinkedList<Rec> {
+fn init_ord_rpt_format() -> LinkedList<Rec> {
 	let mut lst = LinkedList::<Rec>::new();
 	lst.append(&mut init_header());
 	lst.push_back(Rec{ name: "RptSeq",   len: 8 });
@@ -49,65 +26,108 @@ fn init_v2_rpt_format() -> LinkedList<Rec> {
 	lst.push_back(Rec{ name: "DstBrkNo", len: 7 });
 	lst.push_back(Rec{ name: "OrdNo",    len: 20 });
 	lst.push_back(Rec{ name: "PreOrder", len: 1 });
-	lst.push_back(Rec{ name: "FuncCmd",  len: 1 });
-	lst.push_back(Rec{ name: "Symbol", len: 1 });
+	lst.push_back(Rec{ name: "FuncCmd",  len: 2 });
+	lst.push_back(Rec{ name: "Symbol", len: 40 });
 	lst.push_back(Rec{ name: "Side", len: 1 });
 	lst.push_back(Rec{ name: "PriType", len: 1 });
 	lst.push_back(Rec{ name: "PriSgn", len: 1 });
-	lst.push_back(Rec{ name: "Pri99", len: 1 });
-	lst.push_back(Rec{ name: "Qty", len: 1 });
+	lst.push_back(Rec{ name: "Pri99", len: 18 });
+	lst.push_back(Rec{ name: "Qty", len: 8 });
 	lst.push_back(Rec{ name: "SegMkt", len: 1 });
 	lst.push_back(Rec{ name: "FuoPosEff", len: 1 });
 	lst.push_back(Rec{ name: "TIF", len: 1 });
-	lst.push_back(Rec{ name: "SalesNo", len: 1 });
+	lst.push_back(Rec{ name: "SalesNo", len: 8 });
 	lst.push_back(Rec{ name: "IvacKind", len: 1 });
-	lst.push_back(Rec{ name: "OrdYYYYMMDD", len: 1 });
-	lst.push_back(Rec{ name: "OrdHHMMSS", len: 1 });
-	lst.push_back(Rec{ name: "IDNO", len: 1 });
-	lst.push_back(Rec{ name: "OrdKey", len: 1 });
+	lst.push_back(Rec{ name: "OrdYYYYMMDD", len: 8 });
+	lst.push_back(Rec{ name: "OrdHHMMSS", len: 6 });
+	lst.push_back(Rec{ name: "IDNO", len: 16 });
+	lst.push_back(Rec{ name: "OrdKey", len: 64 });
 	lst.push_back(Rec{ name: "PlatFlag", len: 1 });
 	lst.push_back(Rec{ name: "SrcFlag", len: 1 });
 	lst.push_back(Rec{ name: "OrgIvacKind", len: 1 });
-	lst.push_back(Rec{ name: "OrgOrdYYYYMMDD", len: 1 });
-	lst.push_back(Rec{ name: "OrgOrdKey", len: 1 });
+	lst.push_back(Rec{ name: "OrgOrdYYYYMMDD", len: 14 });
+	lst.push_back(Rec{ name: "OrgOrdKey", len: 64 });
 	lst.push_back(Rec{ name: "OrgPlatFlag", len: 1 });
 	lst.push_back(Rec{ name: "OrgSrcFlag", len: 1 });
-	lst.push_back(Rec{ name: "OrgOrdQty", len: 1 });
-	lst.push_back(Rec{ name: "LeavesQty", len: 1 });
-	lst.push_back(Rec{ name: "TMatQty", len: 1 });
-	lst.push_back(Rec{ name: "TDelQty", len: 1 });
-	lst.push_back(Rec{ name: "UDelQty", len: 1 });
-	lst.push_back(Rec{ name: "ErrCode", len: 1 });
-	lst.push_back(Rec{ name: "ErrMsg", len: 1 });
+	lst.push_back(Rec{ name: "OrgOrdQty", len: 8 });
+	lst.push_back(Rec{ name: "LeavesQty", len: 8 });
+	lst.push_back(Rec{ name: "TMatQty", len: 8 });
+	lst.push_back(Rec{ name: "TDelQty", len: 8 });
+	lst.push_back(Rec{ name: "UDelQty", len: 8 });
+	lst.push_back(Rec{ name: "ErrCode", len: 4 });
+	lst.push_back(Rec{ name: "ErrMsg", len: 80 });
 	lst.push_back(Rec{ name: "ExchgMkt", len: 1 });
-	lst.push_back(Rec{ name: "ExchgID", len: 1 });
-	lst.push_back(Rec{ name: "Price2", len: 1 });
-	lst.push_back(Rec{ name: "GTDateYYYYMMDD", len: 1 });
-	lst.push_back(Rec{ name: "BasketID", len: 1 });
-	lst.push_back(Rec{ name: "TERMINAL", len: 1 });
+	lst.push_back(Rec{ name: "ExchgID", len: 10 });
+	lst.push_back(Rec{ name: "Price2", len: 18 });
+	lst.push_back(Rec{ name: "GTDateYYYYMMDD", len: 8 });
+	lst.push_back(Rec{ name: "BasketID", len: 16 });
 	lst
 }
 
 #[allow(dead_code)]
-fn struct_len(lst: &LinkedList<Rec>) -> u32 {
-	let mut sz : u32 = 0;
+fn init_deal_rpt_format() -> LinkedList<Rec> {
+	let mut lst = LinkedList::<Rec>::new();
+	lst.append(&mut init_header());
+    lst.push_back(Rec{ name: "RptSeq", len: 8 });
+    lst.push_back(Rec{ name: "BrkNo", len: 7 });
+    lst.push_back(Rec{ name: "IvacNo", len: 7 });
+    lst.push_back(Rec{ name: "OrdNo", len: 20 });
+    lst.push_back(Rec{ name: "CombFlag", len: 1 });
+    lst.push_back(Rec{ name: "Symbol", len: 40 });
+    lst.push_back(Rec{ name: "Symbol1", len: 20 });
+    lst.push_back(Rec{ name: "Symbol2", len: 20 });
+    lst.push_back(Rec{ name: "Side", len: 1 });
+    lst.push_back(Rec{ name: "SideLeg1", len: 1 });
+    lst.push_back(Rec{ name: "SideLeg2", len: 1 });
+    lst.push_back(Rec{ name: "OrgOrdPriType", len: 1 });
+    lst.push_back(Rec{ name: "OrgOrdSgnPri", len: 19 });
+    lst.push_back(Rec{ name: "SegMkt", len: 1 });
+    lst.push_back(Rec{ name: "FuoPosEff", len: 1 });
+    lst.push_back(Rec{ name: "TIF", len: 1 });
+    lst.push_back(Rec{ name: "SalesNo", len: 8 });
+    lst.push_back(Rec{ name: "IvacKind", len: 1 });
+    lst.push_back(Rec{ name: "OrdYMD", len: 8 });
+    lst.push_back(Rec{ name: "OrdHMS", len: 6 });
+    lst.push_back(Rec{ name: "IDNO", len: 16 });
+    lst.push_back(Rec{ name: "OrgOrdKey", len:  64 });
+    lst.push_back(Rec{ name: "OrgPlatFlag", len: 1 });
+    lst.push_back(Rec{ name: "OrgSrcFlag", len: 1 });
+    lst.push_back(Rec{ name: "OrgOrdQty", len: 8 });
+    lst.push_back(Rec{ name: "LeavesQty", len: 8 });
+    lst.push_back(Rec{ name: "TMatQty", len: 8 });
+    lst.push_back(Rec{ name: "TDelQty", len: 8 });
+    lst.push_back(Rec{ name: "MatYMD", len:  8 });
+    lst.push_back(Rec{ name: "MatHMS", len:  6 });
+    lst.push_back(Rec{ name: "MatQty", len:  8 });
+    lst.push_back(Rec{ name: "MatSgnPri", len: 19 });
+    lst.push_back(Rec{ name: "MatPri1", len: 18 });
+    lst.push_back(Rec{ name: "MatPri2", len: 18 });
+    lst.push_back(Rec{ name: "AvgSgnPri", len: 19 });
+    lst.push_back(Rec{ name: "AvgPri1", len: 18 });
+    lst.push_back(Rec{ name: "AvgPri2", len: 18 });
+    lst.push_back(Rec{ name: "BrkRptSeq", len: 6 });
+    lst.push_back(Rec{ name: "MktRptSeq", len: 8 });
+    lst.push_back(Rec{ name: "ExchgMkt", len: 1 });
+    lst.push_back(Rec{ name: "ExchgID", len: 10 });
+    lst.push_back(Rec{ name: "Price2", len: 18 });
+    lst.push_back(Rec{ name: "GTYMD", len: 8 });
+    lst.push_back(Rec{ name: "BasketID", len: 16 });
+	lst
+}
+
+#[allow(dead_code)]
+fn struct_len(lst: &LinkedList<Rec>) -> usize {
+	let mut sz : usize = 0;
 	for field in lst {
 		sz = sz + field.len;
 	}
 	sz
 }
 
-#[allow(dead_code)]
-fn init_v2_deal_format() -> LinkedList<Rec> {
-	let mut lst = LinkedList::<Rec>::new();
-	lst.append(&mut init_header());
-	lst
-}
-
 /* Parser本體宣告, 內含HTS Log欄位結構的LinkList */
 pub struct Parser {
-	hts_v2_format     : LinkedList<Rec>,
-	hts_v2_format_pos : Vec<FieldPos>,
+	hts_ord_rpt_format   : LinkedList<Rec>,
+	hts_deal_rpt_format  : LinkedList<Rec>,
 }
 
 /* Parser方法實作 */
@@ -115,23 +135,51 @@ impl Parser {
 	// 初始化
 	pub fn new() -> Parser {
 		Parser{ 
-			hts_v2_format     : init_v2_ord_format(),
-			hts_v2_format_pos : get_field_pos_vec(&mut init_v2_ord_format()),
+			hts_ord_rpt_format   : init_ord_rpt_format(),
+			hts_deal_rpt_format  : init_deal_rpt_format(),
 		}
 	}
 	// 從log line的標頭來檢查版本，以便使用符合的電文格式
-	pub fn check_version(&self, line: &str) {
+	#[allow(dead_code)]
+	pub fn check_version(&self, line: &str) -> Result<usize, ParseIntError> {
 		if line.len() > 5 {
-			println!("version is {}", &line[1..5]);
+			Ok(line[1..5].parse::<usize>()?)
+		} else {
+			Ok(0)
+		}
+	}
+	// 檢查為委託或成交格式
+	pub fn check_format(&self, line: &str) -> Result<usize, ParseIntError> {
+		if line.len() > 10 {
+			Ok(line[5..9].parse::<usize>()?)
+		} else {
+			Ok(0)
+		}
+	}
+	// 列出欄位 = 值
+	pub fn show_list(&self, line: &str, fmtlist: &LinkedList<Rec>) {
+		let mut beg_pos :usize = 0;
+		let mut end_pos :usize = 0;
+		let linelen = line.len();
+		for recfmt in fmtlist {
+			end_pos = end_pos + recfmt.len;
+			if end_pos > linelen { break; }
+			println!("{}={}", recfmt.name, &line[beg_pos..end_pos]);
+			beg_pos = end_pos;
 		}
 	}
 	// 輸入一行log
 	pub fn parse_line(&self, line: &str) {
-		for recfmt in &self.hts_v2_format {
-			println!("{} has len {}", recfmt.name, recfmt.len);
+		println!("\n======Origin======\n{}:\n======Parsed======", line);
+		if let Ok(format) = self.check_format(line) {
+			match format {
+				1011 => self.show_list(line, &self.hts_ord_rpt_format),
+				1021 => self.show_list(line, &self.hts_deal_rpt_format),
+				9001 => (),
+				9002 => (),
+				_ => println!("unknown format!"),
+			}
 		}
-		println!("we have line: {}", line);
-		self.check_version(line);
 	}
 	// 取得欄位名稱
 	#[allow(dead_code)]
